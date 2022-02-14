5841a682655f3af8d241161ac9f0cb17a92c6c77
Deepanshi Maheshwari <84969580+Deepanshi9136@users.noreply.github.com>
2022-02-14 18:33:43 +0530

    Create strings2.cpp

---
+++ strings2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+   int main() {
+int a, b;
+string S1[a];
+string S2[b];
+char c = S1[0];
+char d = S2[0];
+string S3 = S1+S2;
+cin >> string S1;
+cin >> string S2;
+int len1 = S1.size();
+int len2 = S2.size();
+cout << len1 << " "<< len2 << '\n' ;
+cout << "S3" << '\n';
+S1[0] = char d;
+S2[0] = char c;
+cout << "S1" << " " << "S2" ;
+
+
+  
+    return 0;
+}
