0465e57039fa1cd4deb485a6a0d484d073fa52dc
Deepanshi Maheshwari <84969580+Deepanshi9136@users.noreply.github.com>
2022-02-14 18:33:03 +0530

    Create strings1.cpp

---
+++ strings1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{ 
+    int n;
+    cin >> n;
+    cin >> string words [n]= {};
+    for (int i=0 ; i<= n ;i++ ){
+        if (i==n){
+            break;
+        }
+        cout <<i<< "\n"
+    }
+
+
+    return 0;
+}
